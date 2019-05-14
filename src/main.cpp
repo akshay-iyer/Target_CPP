5c1f096c2bcd1bca2b1f247c8fe1f87e0ba84735
akshay-iyer <akshay.iyerr@gmail.com>
2019-05-14 11:51:14 -0400

    test commit after ssh

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,17 @@ int main(){
 //    cout<<s.singleNonDuplicate(nums);
 
     //Q 515
-    largestElement l;
-    vector<int> result;
-    TreeNode* root = new TreeNode(4);
-    root->left = new TreeNode(9);
-    root->right = new TreeNode(2);
-    root->left->left = new TreeNode(3);
-    root->left->right = new TreeNode(5);
-    root->right->right = new TreeNode(7);
-    result = l.largestValue(root);
-    for (auto element : result)
-        cout<<element<<" ";
+//    largestElement l;
+//    vector<int> result;
+//    TreeNode* root = new TreeNode(4);
+//    root->left = new TreeNode(9);
+//    root->right = new TreeNode(2);
+//    root->left->left = new TreeNode(3);
+//    root->left->right = new TreeNode(5);
+//    root->right->right = new TreeNode(7);
+//    result = l.largestValue(root);
+//    for (auto element : result)
+//        cout<<element<<" ";
 
 
     return 0;
